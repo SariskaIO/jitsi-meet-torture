c4c5f9975b2c42746ca6ada55a6a8c1e2a3c47d2
Дамян Минков <damencho@jitsi.org>
2021-09-23 08:44:44 -0500

    fix: Fixes raise hand test and NPE.

--- src/test/java/org/jitsi/meet/test/pageobjects/web/Toolbar.java
+++ src/test/java/org/jitsi/meet/test/pageobjects/web/Toolbar.java
@@ -87,11 +87,6 @@ public class Toolbar
     public Toolbar(WebParticipant participant)
     {
         this.participant = Objects.requireNonNull(participant, "participant");
-
-        if (MeetUtils.areReactionsEnabled(this.participant.getDriver()))
-        {
-            reactionsEnabled = true;
-        }
     }
 
     /**
@@ -103,24 +98,10 @@ public class Toolbar
     }
 
     /**
-     * Clicks on the raise hand toolbar button and opens the reactions menu.
-     */
-    public String clickOpenReactionsMenuButton()
-    {
-        return clickButton(REACTIONS_MENU);
-    }
-
-
-    /**
      * Clicks on the raise/lower hand button that enables participants will to speak.
      */
     public String clickRaiseHandButton()
     {
-        if (areReactionsEnabled())
-        {
-            return clickOpenReactionsMenuButton();
-        }
-
         return clickButton(RAISE_HAND);
     }
 
@@ -524,14 +505,5 @@ public class Toolbar
         this.closeOverflowMenu();
 
         return count;
-    }
-
-    /**
-     * Whether reactions are enabled, that changes the raise hand button behaviour.
-     * @return <tt>true</tt> if reactions are enabled.
-     */
-    public boolean areReactionsEnabled()
-    {
-        return reactionsEnabled;
     }
 }
